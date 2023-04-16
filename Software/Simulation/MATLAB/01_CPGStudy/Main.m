94d7b27dd4ca7549c8d4d659d3bc42e47bfdb199
Avinash Baskaran <azb0180@auburn.edu>
2023-04-16 13:22:58 -0500

    adding NeuroMechFly sim

---
+++ Software/Simulation/MATLAB/01_CPGStudy/Main.m
@@ -0,0 +1,94 @@
+clear all; clc
+
+
+initializeRobotParameters
+
+mdl = "rlQuadrupedRobot";
+
+
+numObs = 44;
+obsInfo = rlNumericSpec([numObs 1]);
+obsInfo.Name = "observations";
+numAct = 8;
+actInfo = rlNumericSpec([numAct 1],LowerLimit=-1,UpperLimit= 1);
+actInfo.Name = "torque";
+
+blk = mdl + "/RL Agent";
+env = rlSimulinkEnv(mdl,blk,obsInfo,actInfo);
+
+env.ResetFcn = @quadrupedResetFcn;
+
+rng(0)
+
+createNetworks
+
+
+plot(criticNetwork)
+
+plot(actorNetwork)
+
+agentOptions = rlDDPGAgentOptions();
+agentOptions.SampleTime = Ts;
+agentOptions.DiscountFactor = 0.99;
+agentOptions.MiniBatchSize = 128;
+agentOptions.ExperienceBufferLength = 1e6;
+agentOptions.TargetSmoothFactor = 1e-3;
+agentOptions.NoiseOptions.MeanAttractionConstant = 0.15;
+agentOptions.NoiseOptions.Variance = 0.1;
+
+
+agentOptions.ActorOptimizerOptions.Algorithm = "adam";
+agentOptions.ActorOptimizerOptions.LearnRate = 1e-4;
+agentOptions.ActorOptimizerOptions.GradientThreshold = 1;
+agentOptions.ActorOptimizerOptions.L2RegularizationFactor = 1e-5;
+
+agentOptions.CriticOptimizerOptions.Algorithm = "adam";
+agentOptions.CriticOptimizerOptions.LearnRate = 1e-3;
+agentOptions.CriticOptimizerOptions.GradientThreshold = 1;
+agentOptions.CriticOptimizerOptions.L2RegularizationFactor = 2e-4;
+
+
+
+agent = rlDDPGAgent(actor,critic,agentOptions);
+
+
+trainOpts = rlTrainingOptions(...
+    MaxEpisodes=1000,...
+    MaxStepsPerEpisode=floor(Tf/Ts),...
+    ScoreAveragingWindowLength=250,...
+    Plots="training-progress",...
+    StopTrainingCriteria="AverageReward",...
+    StopTrainingValue=210);
+
+
+trainOpts.UseParallel = true;                    
+trainOpts.ParallelizationOptions.Mode = "async";
+trainOpts.ParallelizationOptions.DataToSendFromWorkers = "Experiences";
+
+
+doTraining = false;
+if doTraining    
+    % Train the agent.
+    trainingStats = train(agent,env,trainOpts);
+else
+    % Load pretrained agent parameters for the example.
+    load("rlQuadrupedAgentParams.mat","params")
+    setLearnableParameters(agent, params);
+end
+
+
+
+
+
+
+%% Simulate
+
+rng(0)
+
+simOptions = rlSimulationOptions(MaxSteps=floor(Tf/Ts));
+experience = sim(env,agent,simOptions);
+
+
+
+
+
